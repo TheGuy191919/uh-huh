82c7488df078b7d9dbaf4458399f25fc1ab688d2
TheGuy191919 <evan_1998@sina.com>
2014-10-14 21:33:26 -0400

    now is able to listen and send message

---
+++ uh-huh/src/main/java/io/github/theguy191919/uhhuh/Uhhuh.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package io.github.theguy191919.uhhuh;
+
+import io.github.theguy191919.udpft.net.ByteReceiver;
+import io.github.theguy191919.udpft.net.ByteSender;
+import io.github.theguy191919.udpft.protocol.Protocol;
+import io.github.theguy191919.udpft.protocol.Protocol0;
+import io.github.theguy191919.udpft.protocol.ProtocolEventListener;
+
+/**
+ *
+ * @author Yiwen Dong
+ */
+public class Uhhuh implements ProtocolEventListener{
+    
+    public void methods(){
+        ByteSender sender = new ByteSender();
+        sender.joinGroup();
+        Protocol protocol = new Protocol0();
+        protocol.setContent("thisd might work");
+        protocol.setSender("sender");
+        protocol.setRecipient("receiver");
+        protocol.sendMessage(sender);
+        Protocol.getProtocolEventHandler().regesterListner(this);
+        ByteReceiver receiver = new ByteReceiver();
+        receiver.startReceiver();
+    }
+    
+    public static void main(String[] args){
+        Uhhuh self = new Uhhuh();
+        self.methods();
+    }
+
+    @Override
+    public void gotEvent(Protocol protocol) {
+        System.out.println(protocol.getContent());
+    }
+}
